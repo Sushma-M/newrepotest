3a5a65afea7a851639e312d117bca50ca604484e
Sushma Maddirala <sushma.maddirala@wavemaker.com>
2017-10-25 06:12:08 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "PRojUsingProjshell",
+  "homePage" : "Main",
+  "name" : "PRojUsingProjshell",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
